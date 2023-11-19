4922428b46c513181f4fbe9b54c044504a0d7d46
Guangying135 <936485851@qq.com>
2023-11-20 01:15:44 +0800

    init

--- src/api/article.js
+++ src/api/article.js
@@ -1,6 +1,6 @@
 import request from '@/utils/request'
 
-// 分类：获取文章分类dsafasdf
+// 分类：获取文章分类
 export const artGetChannelsService = () => request.get('/my/cate/list')
 // 分类：添加文章分类
 export const artAddChannelService = (data) => request.post('/my/cate/add', data)
